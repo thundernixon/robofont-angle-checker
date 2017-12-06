9fbb028ca4dee783e0f5143fe75cc0acce3b1725
Stephen Nixon <swr410@gmail.com>
2017-12-06 12:33:59 +0100

    setup angle checker with class

---
+++ angle-checker_and_drawer.py
@@ -0,0 +1,48 @@
+from math import *
+
+class AngleChecker():
+
+    def __init__(self):    
+        self.g = CurrentGlyph()
+
+    def checkAngles(self):
+        for contour in self.g:
+            prevPoint = contour[-1].points[-1]
+            for seg in contour:
+                if seg.type == "line":
+                    currentPoint = seg.points[-1]
+                    x1, y1 = prevPoint.x, prevPoint.y
+                    x2, y2 = currentPoint.x, currentPoint.y
+                    dx, dy = x2 - x1, y2 - y1
+            
+                    # lineAngle = abs(degrees(atan2(dy, dx)) + 90)
+            
+                    lineAngle = round(degrees(atan2(dy, dx)),2)
+                    # # if lineAngle < 0:
+                    # #     lineAngle += 90
+            
+                    if lineAngle <= 0:
+                        lineAngleAdjusted = abs(lineAngle + 90)
+                    elif lineAngle > 0:
+                        lineAngleAdjusted = abs(lineAngle - 90)
+            
+                    print x1, ",", y1, "|", x2, ",", y2, "|", "line angle:", lineAngle, "| line angle adj:", lineAngleAdjusted
+                        
+                    # if lineAngle < -15 and lineAngle > -18:
+                    #     currentPoint.selected = True
+            
+                    #### simplify the math with this
+                    tolerance = 1
+                    rangeToCheck = 8
+                    desAngle = 16
+                    diff = abs(desAngle - lineAngleAdjusted)
+                    if diff < tolerance:
+                        print diff, "fine"
+                    elif diff < rangeToCheck:
+                        print seg.points, diff, "not fine"
+                    else:
+                        pass
+                prevPoint = seg.points[-1]
+            print "\n"
+    
+AngleChecker().checkAngles()
