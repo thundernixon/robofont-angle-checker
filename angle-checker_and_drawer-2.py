e41fba1bb4777f2698dfeee7060f06dd56f942c7
Stephen Nixon <swr410@gmail.com>
2017-12-13 13:32:54 +0100

    it's working

--- angle-checker_and_drawer-2.py
+++ angle-checker_and_drawer-2.py
@@ -1,5 +1,7 @@
 from AppKit import *
 from vanilla import *
+# import vanilla
+from defconAppKit.windows.baseWindow import BaseWindowController
 
 from lib.UI.stepper import SliderEditIntStepper
 
@@ -25,13 +27,15 @@ class AngleChecker():
         
         y += 30
         self.w.rangeToCheckText = TextBox((10, y, 140, 22), "Range to check +/-:", alignment="right")
-        self.w.rangeToCheck = SliderEditIntStepper((160, y, -10, 22), 8, callback=self.changedCallback, minValue=0, maxValue=45)
+        self.w.rangeToCheck = SliderEditIntStepper((160, y, -10, 22), 12, callback=self.changedCallback, minValue=0, maxValue=45)
         
+        # self.setUpBaseWindowBehavior()
         self.w.open()
         self.w.bind("close", self.windowCloseCallback)
         addObserver(self, "myDrawCallback", "draw")
         addObserver(self, "myDrawCallback", "drawInactive")
         addObserver(self, "myDrawCallback", "spaceCenterDraw")
+        UpdateCurrentGlyphView()
         
         self.g = CurrentGlyph()
         self.badAngles = []
@@ -47,9 +51,13 @@ class AngleChecker():
         removeObserver(self, "draw")
         removeObserver(self, "drawInactive")
         removeObserver(self, "spaceCenterDraw")
-        self.g.update()
+        UpdateCurrentGlyphView()
+        # super(SimpleWindowObserver, self).windowCloseCallback(sender)
+        # self.g.update()
 
     def checkAngles(self):
+        self.badAngles = []
+        self.goodAngles = []
         for contour in self.g:
             prevPoint = contour[-1].points[-1]
             for seg in contour:
@@ -70,76 +78,54 @@ class AngleChecker():
                     elif lineAngle > 0:
                         lineAngleAdjusted = abs(lineAngle - 90)
             
-                    print x1, ",", y1, "|", x2, ",", y2, "|", "line angle:", lineAngle, "| line angle adj:", lineAngleAdjusted
+                    # print x1, ",", y1, "|", x2, ",", y2, "|", "line angle:", lineAngle, "| line angle adj:", lineAngleAdjusted
                         
-                    # if lineAngle < -15 and lineAngle > -18:
-                    #     currentPoint.selected = True
             
                     #### simplify the math with this
-                    # tolerance = 1
                     tolerance = self.w.tolerance.get()
-                    # rangeToCheck = 8
                     rangeToCheck = self.w.rangeToCheck.get()
-                    # desAngle = 16
                     desAngle = self.w.desiredAngle.get()
+                    
                     diff = abs(desAngle - lineAngleAdjusted)
                     if diff < tolerance:
-                        print diff, "fine"
+                        # print diff, "fine"
                         self.goodAngles.append((x1, y1, x2, y2))
-                        # self.draw((0,0,1))
+
                     elif diff < rangeToCheck:
-                        print seg.points, diff, "not fine"
+                        # print seg.points, diff, "not fine"
                         self.badAngles.append((x1, y1, x2, y2))
-                        # self.draw((1,0,0))
+
                     else:
                         pass
                 prevPoint = seg.points[-1]
-            print "\n"
+            # print "\n"
             # print self.badAngles
+            # self.myDrawCallback(self)
+            
+    def refreshCanvas(self):
+        pass
+        # removeObserver(self, "draw")
+        # addObserver(self, "myDrawCallback", "draw")
             
     def myDrawCallback(self, notification):
+        self.refreshCanvas()
+        self.checkAngles()
         glyph = notification["glyph"]
         # # scale = notification["scale"]
-        # color = self.w.color.get()
-        # width = self.w.width.get()
-        
-        fill(1, 0, 0, 0.5)
-        # for c in glyph:
-        #     for p in c.points:
-        #         oval(p.x, p.y, 10, 10)
+
+        lineCap("round")
                 
         for angle in self.badAngles:
-            
-            stroke(1,0,0,.5)
+            stroke(1,0,0,.25)
             strokeWidth(6)
-            # line(41, 442, 30, 403)
-            # print self.badAngles[angle]
-            # print angle[0], angle[1],angle[2],angle[3]
             line(angle[0], angle[1],angle[2],angle[3])
+            
         for angle in self.goodAngles:
-            
-            stroke(0,1,.5,.5)
+            stroke(0,1,.5,.25)
             strokeWidth(4)
-            # line(41, 442, 30, 403)
-            # print self.badAngles[angle]
-            # print angle[0], angle[1],angle[2],angle[3]
             line(angle[0], angle[1],angle[2],angle[3])
-            
-        # if 0:
-        #     lineCap = self._lineCapStylesMap[self.w.lineCap.getTitle()]
-        #     lineJoin = self._lineJoinStylesMap[self.w.lineJoin.getTitle()]
-        
-        #     #path = glyph.naked().getRepresentation("defconAppKit.NSBezierPath")       
-        #     pen = CocoaPen(glyph.getParent())
-        #     glyph.draw(pen)
-        #     path = pen.path
-        
-        #     path.setLineWidth_(width)
-        #     path.setLineCapStyle_(lineCap)
-        #     path.setLineJoinStyle_(lineJoin)
-        
-        #     color.set()
-        #     path.stroke()   
 
 
-AngleChecker().checkAngles()
+# AngleChecker().checkAngles()
+AngleChecker()
+# AngleChecker().myDrawCallback()
